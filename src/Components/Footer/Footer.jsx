54bb61ab5caeb92e69b84f78907ec3f6e1e8f260
asifmahmud211 <20145427@students.latrobe.edu.au>
2021-08-09 19:46:59 +1000

    Mobilr view update

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -3,17 +3,17 @@ import "./footer.css";
 import logo from "../../assets/images/latrobeuni-partnership.jpg"
 const Footer = () => {
     return ( <div className="footer"> 
-    <div className="logo"> 
+    <div className="footer-logo"> 
 <div className="logo-wrapper">
  <a href="https://www.latrobe.edu.au/" target="_blank"><img src={logo} alt="footer-logo"/></a>
 </div>
     </div>
 <div className="terms-use">
 <span> Terms of use </span> 
-<span> Privacy policy</span>
+<span> Privacy Policy</span>
 </div>
 <div className="copyright">
-2021 rights reserved 
+2021 Rights Reserved 
 </div>
     </div>  );
 }
