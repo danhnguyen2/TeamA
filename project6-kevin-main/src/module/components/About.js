2433572b438922fffaab83a568cc1dbed5d1ab41
asifmahmud211 <20145427@students.latrobe.edu.au>
2021-10-06 17:11:10 +1100

    Ready for usability test

---
+++ project6-kevin-main/src/module/components/About.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import './About.css';
+import image from "../../images/image.jpeg"
+
+const About = () => {
+    return (
+        <div className="container-fluid">
+            <h2>About Us</h2>
+            <div>
+                <img src={image} alt="potter" className="about-image" />
+            </div>
+            <p className="about-content">THE TRENDALL RESEARCH CENTRE HAS THE FOLLOWING OBJECTIVES:
+                <br /> <br />
+                TO PROMOTE RESEARCH IN THE GENERAL AREA OF ANCIENT MEDITERRANEAN
+                STUDIES, PARTICULARLY IN THE ARCHAEOLOGY OF SOUTH ITALY AND SICILY DURING
+                THE CLASSICAL PERIOD. <br /> <br />
+
+                TO DISSEMINATE WITHIN THE GENERAL COMMUNITY IN AUSTRALIA THE RESULTS OF
+                THE LATEST RESEARCH IN GREEK AND ROMAN ART AND ARCHAEOLOGY THROUGH
+                THE SPONSORSHIP OF CONFERENCES, LECTURES AND SEMINARS.<br /> <br />
+
+                TO MAKE AVAILABLE, AT THE DIRECTOR'S DISCRETION, THE RESOURCES OF THE
+                CENTRE TO ALL SCHOLARS AND GRADUATE STUDENTS, WHETHER FROM AUSTRALIA
+                OR OVERSEAS, WHO WISH TO USE THE LIBRARY AND ARCHIVE. <br /> <br />
+
+                TO MAINTAIN AND EXTEND BOTH THE LIBRARY AND THE ARCHIVE (AS UNIQUE
+                RESEARCH RESOURCES IN AUSTRALIA) THROUGH THE ACQUISITION OF BOOKS AND
+                PERIODICALS RELATING TO GREEK AND ROMAN CULTURE, AND IMAGES OF SOUTH
+                ITALIAN RED-FIGURE VASES.
+            </p>
+
+
+
+            {/*}    <div className="clearfix">
+                <p className="column menu">THE TRENDALL RESEARCH CENTRE HAS THE FOLLOWING OBJECTIVES:
+                    <br /> <br />
+                    TO PROMOTE RESEARCH IN THE GENERAL AREA OF ANCIENT MEDITERRANEAN
+                    STUDIES, PARTICULARLY IN THE ARCHAEOLOGY OF SOUTH ITALY AND SICILY DURING
+                    THE CLASSICAL PERIOD. <br /> <br />
+
+                    TO DISSEMINATE WITHIN THE GENERAL COMMUNITY IN AUSTRALIA THE RESULTS OF
+                    THE LATEST RESEARCH IN GREEK AND ROMAN ART AND ARCHAEOLOGY THROUGH
+                    THE SPONSORSHIP OF CONFERENCES, LECTURES AND SEMINARS.<br /> <br />
+
+                    TO MAKE AVAILABLE, AT THE DIRECTOR'S DISCRETION, THE RESOURCES OF THE
+                    CENTRE TO ALL SCHOLARS AND GRADUATE STUDENTS, WHETHER FROM AUSTRALIA
+                    OR OVERSEAS, WHO WISH TO USE THE LIBRARY AND ARCHIVE. <br /> <br />
+
+                    TO MAINTAIN AND EXTEND BOTH THE LIBRARY AND THE ARCHIVE (AS UNIQUE
+                    RESEARCH RESOURCES IN AUSTRALIA) THROUGH THE ACQUISITION OF BOOKS AND
+                    PERIODICALS RELATING TO GREEK AND ROMAN CULTURE, AND IMAGES OF SOUTH
+                    ITALIAN RED-FIGURE VASES.
+                </p>
+
+                <div className="column content" >
+                    <img src={image} alt="potter" />
+                </div>
+                 
+            </div>
+               */}
+        </div>
+
+    )
+}
+
+export default About;
